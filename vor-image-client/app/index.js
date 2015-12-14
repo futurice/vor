8156a8bafc66bc7a37b76a9284cd9c69f09305e5
minevala <mika.nevalainen@futurice.com>
2015-12-14 16:51:21 +0200

    Take picture on button event

--- vor-image-client/app/index.js
+++ vor-image-client/app/index.js
@@ -1,8 +1,10 @@
 'use strict';
 const http = require('http');
-const RaspiCam = require('raspicam');
 const socketIO = require('socket.io-client');
-const { SOCKET_SERVER, CAMERA_OPTIONS }  = require('config');
+const cameraUsb = require('camera-usb');
+const base64 = require('base64-stream');
+const fs = require('fs');
+const { SOCKET_SERVER, SOCKET_EVENT_TYPE, SOCKET_EVENT_ID }  = require('config');
 const log = func => message => console.log(func(message));
 const logError = func => error => console.log(func(error));
 
@@ -19,10 +21,25 @@ app.listen(9000, function () {
 // init socket client connection
 const client = socketIO.connect(SOCKET_SERVER);
 
+const matchesEventType = eventType => new RegExp(SOCKET_EVENT_TYPE).test(eventType);
+const matchesEventId = eventId => new RegExp(SOCKET_EVENT_ID).test(eventId);
+
 client.on('connect', socket => {
-  log(() => `Socket connection to ${SOCKET_SERVER} on ${new Date()}`)();
-  client.on('stream', socket => {
-    log(() => `Socket event photo:pool from ${SOCKET_SERVER} on ${new Date()}`)();
+  log(() => `Socket connection to ${SOCKET_SERVER}`)();
+  client.on('stream', event => {
+    if (matchesEventType(event.type) && matchesEventId(event.id)) {
+      log(() => `Socket event ${SOCKET_EVENT} from ${SOCKET_SERVER}`)();
+      let bufs = [];
+      const saveImageStream = cameraUsb.capture();
+      saveImageStream.on('data', (buffer) => bufs.push(buffer));
+      saveImageStream.on('finish', () => {
+        client.emit('message', {
+          id: 'pool',
+          type: 'pool',
+          image: Buffer.concat(bufs).toString('base64')
+        });
+      });
+    }
   });
 });
 
