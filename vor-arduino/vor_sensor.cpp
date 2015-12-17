c8df8c8fa4846f5f6c85dc0975a2111d112fd400
Kenta Kusumoto <kenta.kusumoto@futurice.com>
2015-12-17 14:36:01 +0200

    Add functions (getters/setters, peek, process) and fields (id, name, description) to vor_sensor.h.

--- vor-arduino/vor_sensor.cpp
+++ vor-arduino/vor_sensor.cpp
@@ -4,23 +4,29 @@
 
 #include "vor_sensor.h"
 
-VorSensor::VorSensor(uint8_t pin, SENSOR_TYPE type) :
-    _pin(pin),
-    _type(type) {
+VorSensor::VorSensor(
+    uint8_t pin,
+    SENSOR_TYPE type,
+    const char* id,
+    const char* name,
+    const char* description) :
+_pin(pin),
+_type(type),
+_id(id),
+_name(name),
+_description(description) {
 
     switch (_type) {
         case ANALOG_INPUT:
             _readFunc = analogRead;
             break;
         case ANALOG_OUTPUT:
-            _writeFunc = analogWrite;
             break;
         case DIGITAL_INPUT:
             _readFunc = digitalRead;
             pinMode(_pin, INPUT);
             break;
         case DIGITAL_OUTPUT:
-            _writeFunc = digitalWrite;
             pinMode(_pin, OUTPUT);
             break;
         case DIGITAL_INPUT_PULLUP:
@@ -29,5 +35,26 @@ VorSensor::VorSensor(uint8_t pin, SENSOR_TYPE type) :
             break;
         default:
             break;
+    }
+}
+
+int VorSensor::read() {
+    _value = (*_readFunc)(_pin);
+    return _value;
+}
+
+float VorSensor::readProcessed() {
+    _processedValue = process(read());
+    return _processedValue;
+}
+
+// TODO: this is ugly
+void VorSensor::write(int value) {
+    if (_type == ANALOG_OUTPUT) {
+        analogWrite(_pin, value);
+    } else if (_type == DIGITAL_OUTPUT) {
+        digitalWrite(_pin, value);
+    } else {
+        return;
     }
 }
--- vor-arduino/vor_sensor.h
+++ vor-arduino/vor_sensor.h
@@ -13,51 +13,67 @@ enum SENSOR_TYPE {
     DIGITAL_INPUT        = 0x10,
     DIGITAL_OUTPUT       = 0x20,
     DIGITAL_INPUT_PULLUP = 0x30
-}
+};
 
 typedef int (*READ_FUNC)(uint8_t);
-typedef void (*WRITE_FUNC)(uint8_t, int);
+typedef void (*WRITE_FUNC)(uint8_t, int); // TODO: this not used, see VorSensor::write()
 
 class VorSensor {
-    public:
-        // pin number, sensor type
-        VorSensor(uint8_t pin, SENSOR_TYPE type);
+public:
+    // pin number, sensor type
+    VorSensor(uint8_t pin, SENSOR_TYPE type, const char* id = "", const char* name = "", const char* description = "");
 
-        // read sensor value and return it
-        // analog sensor returns 0 - 1023
-        // digital sensor returns LOW (0) or HIGH (1)
-        virtual int read() { return (*_readFunc)(); };
+    void setId(const char* id) { _id = id; }
+    const char* getId() { return _id; }
 
-        // read sensor value and return processed/converted value
-        // this function has to be implemented by inheriting class
-        virtual float read() = 0;
+    void setName(const char* name) { _name = name; }
+    const char* getName() { return _name; }
 
-        virtual int peek() { return _value; };
+    void setDescription(const char* description) { _description = description; }
+    const char* getDescription() { return _description; }
 
-        virtual float peek() { return process(_value); };
+    // read sensor value and return it
+    // analog sensor returns 0 - 1023
+    // digital sensor returns LOW (0) or HIGH (1)
+    virtual int read();
 
-        // write to actuator
-        // analog actuator (PWM) accepts 0 - 255
-        // digital actuator accepts LOW (0) or HIGH (1)
-        virtual void write(int value);
+    // read sensor value and return processed/converted value
+    // this function has to be implemented by inheriting class
+    virtual float readProcessed();
 
-        // TODO: need a function to write processed value as well?
+    virtual int peek() { return _value; }
 
-        // process/convert sensor value
-        // this function has to be implemented by inheriting class
-        virtual float process(int value) = 0;
+    virtual float peekProcessed() { return _processedValue; }
 
-        // TODO: need a function to process write value as well?
+    // write to actuator
+    // analog actuator (PWM) accepts 0 - 255
+    // digital actuator accepts LOW (0) or HIGH (1)
+    virtual void write(int value);
 
-    private:
-        VorSensor();
+    // TODO: need a function to write processed value as well?
 
-        READ_FUNC _readFunc;
-        WRITE_FUNC _writeFunc;
+    // process/convert sensor value
+    // this function has to be implemented by inheriting class
+    virtual float process(int value) { return value; }
 
-        uint8_t _pin;
-        SENSOR_TYPE _type;
-        int _value;
+    // TODO: need a function to process write value as well? e.g. convert percentage to write value?
+
+protected:
+
+private:
+    VorSensor();
+
+    READ_FUNC _readFunc;
+    // WRITE_FUNC _writeFunc; // TODO: would like to have something like this
+
+    uint8_t _pin;
+    SENSOR_TYPE _type;
+    int _value;
+    float _processedValue;
+
+    const char* _id;
+    const char* _name;
+    const char* _description;
 };
 
 #endif
