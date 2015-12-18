4b8476ff61dd98b06ef5e27d8f246a071f12b1ac
minevala <mika.nevalainen@futurice.com>
2015-12-18 17:55:31 +0200

    List clients on init in test page

--- vor-backend/app/views/routes/index.js
+++ vor-backend/app/views/routes/index.js
@@ -1,11 +1,9 @@
 'use strict';
-const { STREAM } = require('config');
 
 module.exports = router => {
   return router.get('/', (req, res) => {
     res.render('index', {
-      title: 'Vör - backend',
-      STREAM: STREAM
+      title: 'Vör - backend'
     });
   });
 };
