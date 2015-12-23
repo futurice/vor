23c1461a068a5d022426d7ca8ca5dd6213d27d1b
Luís Ramalho <info@luisramalho.com>
2015-12-23 15:30:59 +0200

    Refactored SharedPreferences manager and toilets fragments

--- vor-android/mobile/src/main/java/com/futurice/hereandnow/fragment/Floor7Fragment.java
+++ vor-android/mobile/src/main/java/com/futurice/hereandnow/fragment/Floor7Fragment.java
@@ -4,28 +4,26 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v4.content.ContextCompat;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.RelativeLayout;
 
-import com.futurice.hereandnow.Constants;
 import com.futurice.hereandnow.R;
+import com.futurice.hereandnow.utils.ToiletUtils;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class Floor7Fragment extends Fragment {
-    private static final String TAG = Floor7Fragment.class.getSimpleName();
-
     @Bind(R.id.toilet7AM) RelativeLayout mToilet7AM;
     @Bind(R.id.toilet7BM) RelativeLayout mToilet7BM;
+
+    public static final String TOILET_7AM_ID = "toilet7am";
+    public static final String TOILET_7BM_ID = "toilet7bm";
 
     Activity mActivity;
 
@@ -33,11 +31,11 @@ public class Floor7Fragment extends Fragment {
     SharedPreferences mToilet7bmSP;
 
     OnSharedPreferenceChangeListener toilet7amListener = (sharedPreferences, key) -> {
-        updateToiletView(sharedPreferences, key, mToilet7AM);
+        ToiletUtils.updateView(sharedPreferences, key, mToilet7AM, getActivity());
     };
 
     OnSharedPreferenceChangeListener toilet7bmListener = (sharedPreferences, key) -> {
-        updateToiletView(sharedPreferences, key, mToilet7BM);
+        ToiletUtils.updateView(sharedPreferences, key, mToilet7BM, getActivity());
     };
 
     public Floor7Fragment() {
@@ -48,8 +46,29 @@ public class Floor7Fragment extends Fragment {
         super.onCreate(savedInstanceState);
 
         mActivity = getActivity();
-        mToilet7amSP = mActivity.getSharedPreferences("toilet7am", Context.MODE_PRIVATE);
-        mToilet7bmSP = mActivity.getSharedPreferences("toilet7bm", Context.MODE_PRIVATE);
+        mToilet7amSP = mActivity.getSharedPreferences(TOILET_7AM_ID, Context.MODE_PRIVATE);
+        mToilet7bmSP = mActivity.getSharedPreferences(TOILET_7BM_ID, Context.MODE_PRIVATE);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_floor_7_toilet, container, false);
+        ButterKnife.bind(this, view);
+
+        mToilet7AM.setOnClickListener(v -> {
+            ToiletUtils.setClickListener(mToilet7amSP.getString(TOILET_7AM_ID, null), getActivity());
+        });
+
+        mToilet7BM.setOnClickListener(v -> {
+            ToiletUtils.setClickListener(mToilet7bmSP.getString(TOILET_7BM_ID, null), getActivity());
+        });
+
+        ToiletUtils.updateView(mToilet7amSP, TOILET_7AM_ID, mToilet7AM, getActivity());
+        ToiletUtils.updateView(mToilet7bmSP, TOILET_7BM_ID, mToilet7BM, getActivity());
+
+        return view;
     }
 
     @Override
@@ -65,41 +84,5 @@ public class Floor7Fragment extends Fragment {
         mToilet7amSP.unregisterOnSharedPreferenceChangeListener(toilet7amListener);
         mToilet7bmSP.unregisterOnSharedPreferenceChangeListener(toilet7bmListener);
         super.onPause();
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater,
-                             ViewGroup container,
-                             Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_floor_7_toilet, container, false);
-        ButterKnife.bind(this, view);
-
-        updateView();
-
-        return view;
-    }
-
-    public void updateView() {
-        updateToiletBackground(mToilet7AM, mToilet7amSP.getBoolean(Constants.RESERVED_KEY, false));
-        updateToiletBackground(mToilet7BM, mToilet7bmSP.getBoolean(Constants.RESERVED_KEY, false));
-    }
-
-    private void updateToiletView(SharedPreferences sharedPreferences, String key, View view) {
-        switch (key) {
-            case Constants.RESERVED_KEY:
-                updateToiletBackground(view, sharedPreferences.getBoolean(key, false));
-                break;
-            case Constants.METHANE_KEY:
-                Log.d(TAG, "Methane level: " + Integer.toString(sharedPreferences.getInt(key, 0)));
-                break;
-            default:
-                break;
-        }
-    }
-
-    public void updateToiletBackground(View view, Boolean reserved) {
-        Drawable freeBg = ContextCompat.getDrawable(getContext(), R.drawable.toilet_free_bg);
-        Drawable takenBg = ContextCompat.getDrawable(getContext(), R.drawable.toilet_taken_bg);
-        view.setBackground(reserved ? takenBg : freeBg);
     }
 }
