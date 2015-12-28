28193272290c75b01ca4ee271b02d91dbd7532ec
Ville Tainio <ville.tainio@futurice.com>
2015-12-28 15:18:13 +0200

    Check that the background service is enabled also in the  wifi broadcast receiver

--- vor-android/mobile/src/main/java/com/futurice/hereandnow/services/WifiBroadcastReceiver.java
+++ vor-android/mobile/src/main/java/com/futurice/hereandnow/services/WifiBroadcastReceiver.java
@@ -3,13 +3,16 @@ package com.futurice.hereandnow.services;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
+import android.preference.PreferenceManager;
 import android.util.Log;
 
 import com.futurice.hereandnow.Constants;
+import com.futurice.hereandnow.activity.SettingsActivity;
 
 public class WifiBroadcastReceiver extends BroadcastReceiver {
     private static final String TAG = "NetworkInfo";
@@ -27,8 +30,13 @@ public class WifiBroadcastReceiver extends BroadcastReceiver {
                     if (info.getSSID().equals(Constants.NETWORK_SSID)) {
                         Log.d(TAG, "Connected to correct Wifi.");
 
+                        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
+                        Boolean serviceEnabled = preferences.getBoolean(SettingsActivity.BACKGROUND_SERVICE_KEY, true);
+
                         // Start the location service.
-                        context.startService(new Intent(context, LocationService.class));
+                        if (serviceEnabled) {
+                            context.startService(new Intent(context, LocationService.class));
+                        }
                     }
                 }
 
