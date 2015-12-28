2b5936bbb536702c50cd5e2b9aa5521f9e9922cc
Kenta Kusumoto <kenta.kusumoto@futurice.com>
2015-12-28 14:05:18 +0200

    Add example sketch for vor_humidity.

---
+++ vor-arduino/examples/humidity/humidity.ino
@@ -0,0 +1,16 @@
+#include "vor_humidity.h"
+#include "vor_led.h"
+
+VorHumidity humidity(A0);
+VorLed led;
+
+void setup() {
+    Serial.begin(9600);
+    while (!Serial);
+}
+
+void loop() {
+    float value = humidity.readProcessed();
+    Serial.println(value);
+    delay(1000);
+}
