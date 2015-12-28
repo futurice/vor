4e3841a9412c34df0f60aced951b89f940e35c83
Luís Ramalho <info@luisramalho.com>
2015-12-28 14:56:18 +0200

    Refactored ToiletMapFragment

--- vor-android/mobile/src/main/java/com/futurice/hereandnow/fragment/ToiletMapFragment.java
+++ vor-android/mobile/src/main/java/com/futurice/hereandnow/fragment/ToiletMapFragment.java
@@ -18,37 +18,26 @@ import android.view.ViewGroup;
 
 import com.futurice.hereandnow.Constants;
 import com.futurice.hereandnow.R;
+import com.futurice.hereandnow.pojo.Toilet;
 import com.futurice.hereandnow.view.MapView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class ToiletMapFragment extends Fragment {
-    private static final String TAG = ToiletMapFragment.class.getSimpleName();
 
     @Bind(R.id.toiletMap) MapView mToiletMapImageView;
 
     Activity mActivity;
 
-    SharedPreferences mToilet7amSP;
-    SharedPreferences mToilet7bmSP;
-
-    SharedPreferences mToilet8amSP;
-    SharedPreferences mToilet8bmSP;
-    SharedPreferences mToilet8cmSP;
-    SharedPreferences mToilet8awSP;
-    SharedPreferences mToilet8bwSP;
-    SharedPreferences mToilet8cwSP;
-
-    OnSharedPreferenceChangeListener toilet7amListener = (sharedPreferences, key) -> updateView7th();
-    OnSharedPreferenceChangeListener toilet7bmListener = (sharedPreferences, key) -> updateView7th();
-
-    OnSharedPreferenceChangeListener toilet8amListener = (sharedPreferences, key) -> updateView8th();
-    OnSharedPreferenceChangeListener toilet8bmListener = (sharedPreferences, key) -> updateView8th();
-    OnSharedPreferenceChangeListener toilet8cmListener = (sharedPreferences, key) -> updateView8th();
-    OnSharedPreferenceChangeListener toilet8awListener = (sharedPreferences, key) -> updateView8th();
-    OnSharedPreferenceChangeListener toilet8bwListener = (sharedPreferences, key) -> updateView8th();
-    OnSharedPreferenceChangeListener toilet8cwListener = (sharedPreferences, key) -> updateView8th();
+    ArrayList<Toilet> mToilets = new ArrayList<>();
+    Toilet m7am, m7bm, m8am, m8aw, m8bm, m8bw, m8cm, m8cw;
+    ArrayList<OnSharedPreferenceChangeListener> mOnSharedPreferenceChangeListeners = new ArrayList<>();
 
     private int mCurrentFloor;
 
@@ -62,15 +51,40 @@ public class ToiletMapFragment extends Fragment {
         mCurrentFloor = 7;
 
         mActivity = getActivity();
-        mToilet7amSP = mActivity.getSharedPreferences("toilet7am", Context.MODE_PRIVATE);
-        mToilet7bmSP = mActivity.getSharedPreferences("toilet7bm", Context.MODE_PRIVATE);
 
-        mToilet8amSP = mActivity.getSharedPreferences("toilet8am", Context.MODE_PRIVATE);
-        mToilet8bmSP = mActivity.getSharedPreferences("toilet8bm", Context.MODE_PRIVATE);
-        mToilet8cmSP = mActivity.getSharedPreferences("toilet8cm", Context.MODE_PRIVATE);
-        mToilet8awSP = mActivity.getSharedPreferences("toilet8aw", Context.MODE_PRIVATE);
-        mToilet8bwSP = mActivity.getSharedPreferences("toilet8bw", Context.MODE_PRIVATE);
-        mToilet8cwSP = mActivity.getSharedPreferences("toilet8cw", Context.MODE_PRIVATE);
+        m7am = new Toilet(7, "toilet7am");
+        m7bm = new Toilet(7, "toilet7bm");
+
+        m8am = new Toilet(8, "toilet8am");
+        m8aw = new Toilet(8, "toilet8aw");
+        m8bm = new Toilet(8, "toilet8bm");
+        m8bw = new Toilet(8, "toilet8bw");
+        m8cm = new Toilet(8, "toilet8cm");
+        m8cw = new Toilet(8, "toilet8cw");
+
+        mToilets.add(m7am);
+        mToilets.add(m7bm);
+        mToilets.add(m8am);
+        mToilets.add(m8aw);
+        mToilets.add(m8bm);
+        mToilets.add(m8bw);
+        mToilets.add(m8cm);
+        mToilets.add(m8cw);
+
+        for (Toilet toilet : mToilets) {
+            SharedPreferences sp = mActivity.getSharedPreferences(toilet.getId(), Context.MODE_PRIVATE);
+            toilet.setSharedPreferences(sp);
+            updateStatus(toilet);
+            mOnSharedPreferenceChangeListeners.add((sharedPreferences, key) -> {
+                updateStatus(toilet);
+                int floor = toilet.getFloor();
+                if (floor == 7) {
+                    updateView7th();
+                } else if (floor == 8) {
+                    updateView8th();
+                }
+            });
+        }
     }
 
     @Override
@@ -85,28 +99,12 @@ public class ToiletMapFragment extends Fragment {
     @Override
     public void onResume() {
         super.onResume();
-        mToilet7amSP.registerOnSharedPreferenceChangeListener(toilet7amListener);
-        mToilet7bmSP.registerOnSharedPreferenceChangeListener(toilet7bmListener);
-
-        mToilet8amSP.registerOnSharedPreferenceChangeListener(toilet8amListener);
-        mToilet8bmSP.registerOnSharedPreferenceChangeListener(toilet8bmListener);
-        mToilet8cmSP.registerOnSharedPreferenceChangeListener(toilet8cmListener);
-        mToilet8awSP.registerOnSharedPreferenceChangeListener(toilet8awListener);
-        mToilet8bwSP.registerOnSharedPreferenceChangeListener(toilet8bwListener);
-        mToilet8cwSP.registerOnSharedPreferenceChangeListener(toilet8cwListener);
+        registerListeners();
     }
 
     @Override
     public void onPause() {
-        mToilet7amSP.unregisterOnSharedPreferenceChangeListener(toilet7amListener);
-        mToilet7bmSP.unregisterOnSharedPreferenceChangeListener(toilet7bmListener);
-
-        mToilet8amSP.unregisterOnSharedPreferenceChangeListener(toilet8amListener);
-        mToilet8bmSP.unregisterOnSharedPreferenceChangeListener(toilet8bmListener);
-        mToilet8cmSP.unregisterOnSharedPreferenceChangeListener(toilet8cmListener);
-        mToilet8awSP.unregisterOnSharedPreferenceChangeListener(toilet8awListener);
-        mToilet8bwSP.unregisterOnSharedPreferenceChangeListener(toilet8bwListener);
-        mToilet8cwSP.unregisterOnSharedPreferenceChangeListener(toilet8cwListener);
+        unregisterListeners();
         super.onPause();
     }
 
@@ -144,8 +142,8 @@ public class ToiletMapFragment extends Fragment {
     }
 
     public void updateView7th() {
-        Boolean am = !mToilet7amSP.getBoolean(Constants.RESERVED_KEY, true);
-        Boolean bm = !mToilet7bmSP.getBoolean(Constants.RESERVED_KEY, true);
+        Boolean am = !m7am.getStatus();
+        Boolean bm = !m7bm.getStatus();
 
         if (am && bm) {
             setToiletMapImageView(R.drawable.map_toilet_7th_floor_aa_ba);
@@ -159,13 +157,13 @@ public class ToiletMapFragment extends Fragment {
     }
 
     public void updateView8th() {
-        Boolean am = !mToilet8amSP.getBoolean(Constants.RESERVED_KEY, true);
-        Boolean bm = !mToilet8bmSP.getBoolean(Constants.RESERVED_KEY, true);
-        Boolean cm = !mToilet8cmSP.getBoolean(Constants.RESERVED_KEY, true);
+        Boolean am = !m8am.getStatus();
+        Boolean bm = !m8bm.getStatus();
+        Boolean cm = !m8cm.getStatus();
 
-        Boolean aw = !mToilet8awSP.getBoolean(Constants.RESERVED_KEY, true);
-        Boolean bw = !mToilet8bwSP.getBoolean(Constants.RESERVED_KEY, true);
-        Boolean cw = !mToilet8cwSP.getBoolean(Constants.RESERVED_KEY, true);
+        Boolean aw = !m8aw.getStatus();
+        Boolean bw = !m8bw.getStatus();
+        Boolean cw = !m8cw.getStatus();
 
         if (am && aw && bm && bw && cm && cw) {
             setToiletMapImageView(R.drawable.map_toilet_8th_floor_ama_awa_bma_bwa_cma_cwa);
@@ -295,6 +293,35 @@ public class ToiletMapFragment extends Fragment {
             setToiletMapImageView(R.drawable.map_toilet_8th_floor_amt_awt_bmt_bwt_cmt_cwa);
         } else if (!am && !aw && !bm && !bw && !cm && !cw) {
             setToiletMapImageView(R.drawable.map_toilet_8th_floor_amt_awt_bmt_bwt_cmt_cwt);
+        }
+    }
+
+    private void registerListeners() {
+        for(int i = 0; i < mToilets.size(); i++) {
+            SharedPreferences sharedPreferences = mToilets.get(i).getSharedPreferences();
+            OnSharedPreferenceChangeListener listener = mOnSharedPreferenceChangeListeners.get(i);
+            sharedPreferences.registerOnSharedPreferenceChangeListener(listener);
+        }
+    }
+
+    private void unregisterListeners() {
+        for(int i = 0; i < mToilets.size(); i++) {
+            SharedPreferences sharedPreferences = mToilets.get(i).getSharedPreferences();
+            OnSharedPreferenceChangeListener listener = mOnSharedPreferenceChangeListeners.get(i);
+            sharedPreferences.unregisterOnSharedPreferenceChangeListener(listener);
+        }
+    }
+
+    private void updateStatus(Toilet toilet) {
+        SharedPreferences sharedPreferences = toilet.getSharedPreferences();
+        String json = sharedPreferences.getString(toilet.getId(), null);
+        if (json != null) {
+            try {
+                JSONObject jsonData = new JSONObject(json);
+                toilet.setStatus(jsonData.getBoolean(Constants.RESERVED_KEY));
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
